b9d4d5ceb10a4f12a2570b37b859d99e097447c4
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-01-30 19:15:42 +0100

    add store and configure it

--- ArgentBank_front/src/index.js
+++ ArgentBank_front/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
+import { Provider } from 'react-redux';
+import store from './store';
 import reportWebVitals from './reportWebVitals';
  
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
