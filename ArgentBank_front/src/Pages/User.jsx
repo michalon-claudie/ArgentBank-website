33bdb17026ece3de8ce0d93050578fbc19478cb4
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-02-02 18:00:31 +0100

    add the right name on login

--- ArgentBank_front/src/Pages/User.jsx
+++ ArgentBank_front/src/Pages/User.jsx
@@ -31,7 +31,7 @@ function User(){
         <Header/>
         <main class="main bg-dark">
             <div class="header">
-                <h1>Welcome back<br />Tony Jarvis!</h1>
+                <h1>Welcome back<br />{userData.firstName + userData.lastName + "!"}</h1>
                 <button class="edit-button">Edit Name</button>
             </div>
             <h2 class="sr-only">Accounts</h2>
